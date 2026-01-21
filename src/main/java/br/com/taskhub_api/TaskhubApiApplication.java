0090a997b0716b24a47d48249c406d9b47b7336f
brunomota7 <bruno7motadev@gmail.com>
2026-01-21 18:00:26 -0300

    First commit, starting project in GitHub

---
+++ src/main/java/br/com/taskhub_api/TaskhubApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.taskhub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskhubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskhubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/taskhub_api/TaskhubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.taskhub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskhubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
